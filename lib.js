d370e18a625c9dc9a6594289250b74b1f79c291a
ClaytonTaylor <claytaylor@cox.net>
2017-03-13 18:53:43 -0500

    HomePage + Layout initial stage

--- lib.js
+++
@@ -1,9 +0,0 @@
-console.log("JS is Loading");
-//
-// var button = document.querySelector("button");
-// var element = document.querySelector("div");
-//
-// button.addEventListener("click", function(){
-//   element.classList.toggle("change");
-//   // console.log("clicking button")
-// });
