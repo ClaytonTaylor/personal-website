83782f80f49454a0333329e2afab0abf3ab443cb
ClaytonTaylor <claytaylor@cox.net>
2017-03-28 19:11:46 -0500

    Color Transition Complete

--- directory/js/jquery/particle.js
+++ directory/js/jquery/particle.js
@@ -1,9 +1,9 @@
-
+// Dots and Lines
 
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    dotColor: '#6497b1',
+    lineColor: '#6497b1'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
@@ -22,3 +22,24 @@ $(document).ready(function() {
   });
 });
 */
+
+// Change the background color
+
+jQuery(function ($) {
+    function changeColor(selector, colors, time) {
+        /* Params:
+         * selector: string,
+         * colors: array of color strings,
+         * every: integer (in mili-seconds)
+         */
+        var curCol = 0,
+            timer = setInterval(function () {
+                if (curCol === colors.length) curCol = 0;
+                $(selector).css("background-color", colors[curCol]);
+                curCol++;
+            }, time);
+    }
+    $(window).load(function () {
+        changeColor(".container", ["#011f4b", "#03396c", "#005b96"], 10000);
+    });
+});
